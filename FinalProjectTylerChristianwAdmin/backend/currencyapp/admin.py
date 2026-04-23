0d3ffa76cf2969b4d47e4e0a70c5d245c425802c
Tyler Allen <78964741+MrAllen81@users.noreply.github.com>
2026-04-23 14:58:20 -0400

    Change 'phone_number' to 'phonenumber' in admin display

--- FinalProjectTylerChristianwAdmin/backend/currencyapp/admin.py
+++ FinalProjectTylerChristianwAdmin/backend/currencyapp/admin.py
@@ -3,7 +3,7 @@ from .models import RegisteredUser
 
 @admin.register(RegisteredUser)
 class RegisteredUserAdmin(admin.ModelAdmin):
-    list_display  = ['first_name', 'last_name', 'email', 'phone_number', 'dob', 'registered_at']
+    list_display  = ['first_name', 'last_name', 'email', 'phonenumber', 'dob', 'registered_at']
     search_fields = ['first_name', 'last_name', 'email']
     list_filter   = ['registered_at']
     readonly_fields = ['registered_at']
